186c23474de7c2736e02e6d4517a6928004a6494
Marcos Defina <marcosdefina@gmail.com>
2019-07-26 16:29:48 +0300

    bouncing ball

--- exploring.js
+++ exploring.js
@@ -1,44 +1,84 @@
-float joao = 0;
-float y = 100;
-up = true; 
-upS = true; 
+RIGHT = 'RIGHT';
+LEFT = 'LEFT';
+TOP = 'TOP';
+BOTTOM = 'BOTTOM';
 
-inital_coord = [300,100];
+up = true; 
+upS = true;
+position_v = [300, 300];
+velocity_v = [10, 15];
 
 void setup() {
+  frameRate(60);
   size(600, 600);
-  stroke(-150); // set stroke color to yellow
   y = height * 0.5;
 }
 
 void draw() { 
-  if(joao == 255){
-    up = false;
-  }else if ( joao == 0) up = true;
+  background(0); //always redraw background
+    
+  drawCircle(position_v);
+  position_v = moveCircle(position_v, velocity_v);
 
-  if(up)
-    joao += 1;
-  else
-    joao -= 1;
+  velocity_v = newDirection(velocity_v, bounderyDetection(position_v));
 
-  background(150);
-  line(0, y, width, y);  
-  
+
   if(y < 0) upS= false;
   if(y > height) upS= true;
 
-  if(upS) strokeGoUp();
-  else strokeGoDown();
-} 
-
-void strokeGoUp(){
-  y = y - 1; 
+  if(upS) strokeGoUp(10);
+  else strokeGoDown(10);
 }
 
-void strokeGoDown(){
-  y = y + 1; 
+/** This function detects the colition of the circle with the screen bonderies
+  * @param { Array } x - contains the position vector
+ */
+void bounderyDetection(x){
+  if(x[0] > width)
+    return RIGHT;
+  if(x[0] < 0)
+    return LEFT;
+  if(x[1] > height)
+    return TOP;
+  if(x[1] < 0)
+    return BOTTOM;
+  return false;
 }
 
-void mousePressed() {
-  loop();
+/** This function sums the position vector with the velocity vector 
+  * @param { Array } x - contains the position vector;
+  * @param { Array } v - contains the velocity vector;
+ */
+void moveCircle(x, v){
+  x[0] += v[0];
+  x[1] += v[1];
+  
+  return x;
+}
+
+/** This function swaps the velocity components due to the reflection on the incident angle;
+  * @param { Array } v - vector containing x_velocity and y_velocity
+  * @param { Type } colision - contain info about where the colition happend
+ */
+void newDirection(v, colision){
+  if(colision === RIGHT || colision === LEFT)
+      v[0] = -v[0];
+  if(colision === TOP || colision === BOTTOM)
+      v[1] = -v[1];
+  return v;
+}
+
+void drawCircle(v) {
+  stroke(255);
+  strokeWeight(2);
+  fill(127);
+  ellipse(v[0],v[1],48,48);
+}
+
+void strokeGoUp(speed){
+  y = y - speed; 
+}
+
+void strokeGoDown(speed){
+  y = y + speed; 
 }
